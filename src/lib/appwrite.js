09fcb12c3a2bcd7a7827a1dce4936db2430a60bc
Daniel Vitiello <48419913+POWERDANIEL@users.noreply.github.com>
2025-06-03 22:49:04 +0200

    Rename VITE env vars and update usage

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -10,7 +10,7 @@ import { Client, Account, Avatars, Databases } from 'appwrite';
 const client = new Client();
 
 client
-  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID)
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID)
   .setEndpoint('https://cloud.appwrite.io/v1');
 
 /**
--- src/lib/googleAi.js
+++ src/lib/googleAi.js
@@ -4,7 +4,7 @@
  */
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
-const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
 
